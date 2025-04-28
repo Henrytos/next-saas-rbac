c0bcedab3afb4e316aa75dc1b72db2480b5b60cb
Henry <henry.arcaya@sptech.school>
2025-04-28 00:11:29 -0300

    testando condições de permisões

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,14 @@
-import { defineAbilityFor } from '@saas/auth'
+import { defineAbilityFor, projectSchema } from '@saas/auth'
 
-const ability = defineAbilityFor({ id: '1', role: 'MEMBER' })
+const ability = defineAbilityFor({ id: 'user-id', role: 'MEMBER' })
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeletedOtherUsers = ability.can('delete', 'User')
 const userCannotDeletedOtherUsers = ability.cannot('delete', 'User')
+const userCanDeleteProject = ability.can('delete', project)
 
 console.log(userCanInviteSomeoneElse)
 console.log(userCanDeletedOtherUsers)
 console.log(userCannotDeletedOtherUsers)
+console.log(userCanDeleteProject)
