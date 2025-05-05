f2e0c2495100922b36f573b193d4f9af81eea449
Henry <henry.arcaya@sptech.school>
2025-05-04 23:41:09 -0300

    Associação automática via e-mail

--- apps/api/src/http/auth/create-account.ts
+++ apps/api/src/http/auth/create-account.ts
@@ -31,12 +31,28 @@ export async function createAccount(app: FastifyInstance) {
         return reply.status(400).send({ message: 'Email already in use' })
       }
 
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
       const passwordHash = await hash(password, 6)
       await prisma.user.create({
         data: {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       })
 
