d6706f936f06660f289fdd7a82c70ac6619ae58a
Henry <henry.arcaya@sptech.school>
2025-05-09 01:29:25 -0300

    Rota: Autenticação via senha(finalizada)

--- apps/api/src/http/auth/authenticate-with-password.ts
+++ apps/api/src/http/auth/authenticate-with-password.ts
@@ -1,6 +1,7 @@
+import { compare } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
-import z, { string } from 'zod'
+import z from 'zod'
 
 import { prisma } from '../lib/prisma'
 
@@ -9,16 +10,26 @@ export async function authenticateWithPassword(app: FastifyInstance) {
     '/sessions/password',
     {
       schema: {
+        tags: ['auth'],
+        summary: 'authenticate via email & password',
         body: z.object({
           email: z.string().email(),
-          password: z.string(),
+          password: z.string().min(8),
         }),
+        response: {
+          201: z.object({
+            token: z.string(),
+          }),
+          400: z.object({
+            message: z.string(),
+          }),
+        },
       },
     },
     async (request, reply) => {
       const { email, password } = request.body
 
-      const userFromEmail = prisma.user.findUnique({
+      const userFromEmail = await prisma.user.findUnique({
         where: {
           email,
         },
@@ -34,6 +45,29 @@ export async function authenticateWithPassword(app: FastifyInstance) {
           message: 'User does not have a password, use social login.',
         })
       }
+
+      const passwordValid = await compare(password, userFromEmail.passwordHash)
+
+      if (!passwordValid) {
+        return reply.status(400).send({
+          message: 'bad request, password invalid',
+        })
+      }
+
+      const token = await reply.jwtSign(
+        {
+          sub: userFromEmail.id,
+        },
+        {
+          sign: {
+            expiresIn: '7d',
+          },
+        }
+      )
+
+      return reply.status(201).send({
+        token,
+      })
     }
   )
 }
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,4 +1,5 @@
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 import fastify from 'fastify'
@@ -9,6 +10,7 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { authenticateWithPassword } from './auth/authenticate-with-password'
 import { createAccount } from './auth/create-account'
 
 const app = fastify()
@@ -17,6 +19,9 @@ app.withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
 app.register(fastifySwagger, {
   openapi: {
     info: {
@@ -34,6 +39,7 @@ app.register(fastifySwaggerUI, {
 })
 app.register(fastifyCors)
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app
   .listen({
